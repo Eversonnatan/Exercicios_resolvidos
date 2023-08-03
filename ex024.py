3c08f9ecbbed47e50a15410b8060eeeaea8abb25
Eversonnatan <106198940+Eversonnatan@users.noreply.github.com>
2023-08-02 21:57:45 -0300

    "veridica"

--- ex024.py
+++ ex024.py
@@ -1,8 +1,4 @@
 #Crie um programa  que leia o nome de uma cidade e
 #diga se ela começa ou não com o nome "Santo"
-cidade = str(input('Digite o nome da sua Cidade: ')).capitalize().strip()
-ver = cidade.split()
-if ver[0] == 'Santo':
-    print('A sua Cidade começa com Santo')
-else:
-    print('Sua Cidade não começa com Santo')
+cidade = str(input('Em que Cidade Você Nasceu: ')).strip()
+print(cidade[:5].upper() == 'SANTO')
